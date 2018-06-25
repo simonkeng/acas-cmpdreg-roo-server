4ee8905686b7241d098a2752cc762f61ed3573c0
Brian Bolt <brian@mcneilco.com>
2018-06-25 15:57:29 -0700

    increasing migration number

--- src/main/java/com/labsynch/cmpdreg/db/migration/sqlserver/jchem/V1_0_4_5__Add_standardization_jchem_tables.java
+++ src/main/java/com/labsynch/cmpdreg/db/migration/sqlserver/jchem/V1_0_4_5__Add_standardization_jchem_tables.java
@@ -13,9 +13,9 @@ import chemaxon.jchem.db.StructureTableOptions;
 import chemaxon.jchem.db.UpdateHandler;
 import chemaxon.util.ConnectionHandler;
 
-public class V1_0_4_4__Add_standardization_jchem_tables implements JdbcMigration {
+public class V1_0_4_5__Add_standardization_jchem_tables implements JdbcMigration {
  
-	Logger logger = LoggerFactory.getLogger(V1_0_4_4__Add_standardization_jchem_tables.class);
+	Logger logger = LoggerFactory.getLogger(V1_0_4_5__Add_standardization_jchem_tables.class);
 
 	//create the jchem table to store the compounds
 
